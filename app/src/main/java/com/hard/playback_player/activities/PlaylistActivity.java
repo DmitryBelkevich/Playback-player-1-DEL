c7c6b3c1c989bfeba8b1e0605e70c39391dff0fb
hard <harleydav@mail.ru>
2020-12-24 14:18:54 +0300

    ListView icons with: text, score, playback

--- app/src/main/java/com/hard/playback_player/activities/PlaylistActivity.java
+++ app/src/main/java/com/hard/playback_player/activities/PlaylistActivity.java
@@ -1,12 +1,19 @@
 package com.hard.playback_player.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
@@ -18,7 +25,6 @@ import com.hard.playback_player.storage.DatabaseInitializer;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 
 public class PlaylistActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener, AdapterView.OnItemClickListener {
@@ -26,9 +32,9 @@ public class PlaylistActivity extends AppCompatActivity implements SwipeRefreshL
     private ListView playlist_listView;
 
     private SongService songService;
+    private Collection<Song> songs;
 
-    private ArrayAdapter<String> arrayAdapter;
-    private List<String> titles;
+    private ArrayAdapter<Song> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +45,6 @@ public class PlaylistActivity extends AppCompatActivity implements SwipeRefreshL
         playlist_listView = findViewById(R.id.playlist_listView);
 
         songService = new SongService();
-        titles = new ArrayList<>();
 
         swipeRefreshLayout.setOnRefreshListener(this);
         playlist_listView.setOnItemClickListener(this);
@@ -54,35 +59,68 @@ public class PlaylistActivity extends AppCompatActivity implements SwipeRefreshL
         Database.clear();
         DatabaseInitializer.init();
 
-        Collection<Song> songs = songService.getAll();
+        songs = songService.getAll();
 
-        titles.clear();
-
-        Iterator<Song> iterator = songs.iterator();
-        while (iterator.hasNext()) {
-            Song song = iterator.next();
-
-            String songTitle = song.getTitle();
-            String bandTitle = song.getBand().getTitle();
-
-            titles.add(bandTitle + " - " + songTitle);
-        }
-
-        setAdapter();
+        // setAdapter
+        arrayAdapter = new PlaylistAdapter(this, R.layout.playlist_item, (List<Song>) songs);
+        playlist_listView.setAdapter(arrayAdapter);
 
         swipeRefreshLayout.setRefreshing(false);
     }
 
-    private void setAdapter() {
-        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titles);
-        playlist_listView.setAdapter(arrayAdapter);
+    private class PlaylistAdapter extends ArrayAdapter<Song> {
+        private Context context;
+        private int resource;
+
+        public PlaylistAdapter(@NonNull Context context, int resource, @NonNull List<Song> objects) {
+            super(context, resource, objects);
+            this.context = context;
+            this.resource = resource;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+
+            convertView = layoutInflater.inflate(resource, parent, false);
+
+            Song song = getItem(position);
+
+            // title
+
+            TextView songTitle_textView = convertView.findViewById(R.id.songTitle_playlist);
+            songTitle_textView.setText(song.getTitle());
+
+            TextView bandTitle_textView = convertView.findViewById(R.id.bandTitle_playlist);
+            bandTitle_textView.setText(song.getBand().getTitle());
+
+            // icons
+
+            if (song.getText() != null) {
+                ImageView textIcon = convertView.findViewById(R.id.text_icon_playlist);
+                textIcon.setVisibility(View.VISIBLE);
+            }
+
+            if (!song.getScores().isEmpty()) {
+                ImageView scoreIcon = convertView.findViewById(R.id.score_icon_playlist);
+                scoreIcon.setVisibility(View.VISIBLE);
+            }
+
+            if (!song.getPlaybacks().isEmpty()) {
+                ImageView playbackIcon = convertView.findViewById(R.id.playback_icon_playlist);
+                playbackIcon.setVisibility(View.VISIBLE);
+            }
+
+            return convertView;
+        }
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         Intent intent = new Intent(PlaylistActivity.this, SongActivity.class);
 
-        Song song = songService.getById(id + 1);
+        Song song = arrayAdapter.getItem(position);
 
         intent.putExtra("song", song);
 
